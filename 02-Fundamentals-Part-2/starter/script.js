2845259e9c062a69dbcb268070d4a0a52e3b0b79
yousefabughaidah <94529498+yousefabughaidah@users.noreply.github.com>
2021-11-18 14:50:24 +0300

    Update script.js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,42 @@
+/*
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log(`I can drive!`)
+
+const interface = 'Audio'
+
+// Let's talk about functions!
+
+// Declaring the function
+function logger() {
+    console.log('My name is Yousef!');
+}
+
+// calling, running or invoking the function
+logger()
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice
+}
+
+// assigning the function to a variable
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice)
+
+// printing it to console directly
+console.log(fruitProcessor(5, 0));
+
+const appleAndOrangeJuice = fruitProcessor(2, 4);
+console.log(appleAndOrangeJuice);
+
+
+TESTING 123
+
+
+CLOSE OFF HERE ---------------------------------------------------------------------- */
