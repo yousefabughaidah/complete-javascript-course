3c1cd682bf931cf45e252d5e45269720ab9337cb
yousefabughaidah <94529498+yousefabughaidah@users.noreply.github.com>
2021-11-23 15:07:04 +0300

    Starting with the pig game

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// select the elements that you need, and store them into variables
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal'); // querySelectorAll is used to capture all buttons
+
+// functions
+const closeModal = function () {
+    modal.classList.add('hidden'); // we use the .add to add a class to the element
+    overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+    modal.classList.remove('hidden'); // we use the .add to add a class to the element
+    overlay.classList.remove('hidden');
+};
+
+// the code
+// open the modal on clicking the button
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// close the modal when clicking the 'x' or the overlay
+btnCloseModal.addEventListener('click', closeModal); // we are not calling the function, we only want JS to execute the function on the click
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// constants
+const playerOneScoreElement = document.getElementById('score--0');
+const playerTwoScooreElement = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+const rollDiceBtnElement = document.querySelector('.btn--roll');
+let pOneScore = 0;
+let pTwoScore = 0;
+
+// functions
+
+const rollDice = function () {
+    // generate random number from 1 to 6
+    const min = Math.ceil(1);
+    const max = Math.floor(6);
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // show the dice picture corresponding to the random number
+    const dice_img = `dice-${randomNumber}.png`;
+    diceElement.src = dice_img;
+    diceElement.classList.remove('hidden');
+    console.log(`You rolled a ${randomNumber}`);
+};
+
+// Create the starting point for the game
+playerOneScoreElement.textContent = pOneScore;
+playerTwoScooreElement.textContent = pTwoScore;
+diceElement.classList.add('hidden');
+
+// When clicking the 'roll dice button' get a number from 1 - 6 and show the dice
+rollDiceBtnElement.addEventListener('click', rollDice);
+
+// .addEventListener('click', closeModal);
