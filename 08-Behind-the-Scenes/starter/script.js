d7c1385b9be1ffd1f373d540e420e603a32f398a
yousefabughaidah <94529498+yousefabughaidah@users.noreply.github.com>
2021-11-26 21:45:29 +0300

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}. you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Yousef';
+calcAge(1991);
