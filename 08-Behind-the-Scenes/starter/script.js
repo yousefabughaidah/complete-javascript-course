c49093c340f5cad50fb7b1339b022a7ba88c301e
yousefabughaidah <94529498+yousefabughaidah@users.noreply.github.com>
2021-11-26 21:54:50 +0300

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -6,6 +6,14 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName}. you are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);
+    console.log(millenial);
   }
 
   printAge();
