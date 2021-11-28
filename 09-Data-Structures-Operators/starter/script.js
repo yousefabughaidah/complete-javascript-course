25508e768a3a68f1bbb352530d8917fa9a90fa4c
Yousef E. Abu Ghaidah <yghaidah@qf.org.qa>
2021-11-28 15:46:09 +0300

    Finished coding challenge 1 in the Data Structures section

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,35 @@ const restaurant = {
   },
 };
 
+const rest1 = {
+  name: 'Capri',
+  numOfGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Reem',
+};
+
+// use the or operator to add numofguests property to the objects that don't have them
+// console.log(rest1);
+// console.log(rest2);
+
+// // rest1.numOfGuests = rest1.numOfGuests || 10;
+// // rest2.numOfGuests = rest2.numOfGuests || 10;
+
+// // rest1.numOfGuests ||= 10;
+// // rest2.numOfGuests ||= 10;
+
+// // rest1.numOfGuests ??= 10;
+// // rest2.numOfGuests ??= 10;
+
+// rest1.owner &&= 'Anonymous';
+// rest2.owner &&= 'Anonymous';
+
+// console.log(rest1);
+// console.log(rest2);
+
 // let [main, , secondary] = restaurant.categories;
 // console.log(main, secondary);
 
@@ -193,3 +222,143 @@ const restaurant = {
 
 // restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
 // restaurant.orderPizza('mushrooms');
+
+// console.log('----- OR OPERATOR -----');
+
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// const guests2 = restaurant.numGuests || 10;
+
+// console.log(guests1);
+// console.log(guests2);
+
+// console.log('----- AND OPERATOR -----');
+// console.log(0 && 'Yousef');
+// console.log(7 && 'Yousef');
+// console.log('hello' && 23 && null && 'Jonas');
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+
+// // restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// restaurant.numGuests = 0;
+// const guests2 = restaurant.numGuests || 10;
+// const guests3 = restaurant.numGuests ?? 10;
+
+// console.log(guests2);
+// console.log(guests3);
+
+// -----CODING CHALLENGE #1 -------
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+console.log(`---- TASK 1 ----`);
+
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+console.log(`---- TASK 2 ----`);
+const [gk, ...fieldPlayers] = players1;
+
+console.log(gk);
+console.log(fieldPlayers);
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+console.log(`---- TASK 3 ----`);
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+console.log(`---- TASK 4 ----`);
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(playersFinal);
+
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+console.log(`---- TASK 5 ----`);
+const { team1, team2, x: draw } = game.odds;
+console.log(team1);
+console.log(draw);
+console.log(team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+console.log(`---- TASK 6 ----`);
+
+const printGoals = function (...playersWhoScored) {
+  const totalGoals = playersWhoScored.length;
+  console.log(totalGoals);
+  console.log(
+    `The players who scored are:${playersWhoScored} with ${totalGoals} goals!`
+  );
+};
+
+printGoals(...game.scored);
+
+// TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+console.log(`---- TASK 7 ----`);
+// I TRIED AND FAILED LETS LEARN.
+team1 < team2 && console.log('Team 1 is more likely to win.');
+team2 < team1 && console.log('Team 2 is more likely to win.');
+
+// Here's a better way to do it:
+console.log(
+  `The winner is ${
+    (team1 < team2 && game.team1) || (team2 < team1 && game.team2)
+  }`
+);
