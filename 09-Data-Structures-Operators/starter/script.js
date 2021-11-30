20b04a34adba4a0d9f1af7bdb62e37968b2fc860
yousefabughaidah <94529498+yousefabughaidah@users.noreply.github.com>
2021-11-30 22:30:54 +0300

    Update on closing the section

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -658,30 +658,60 @@ GOOD LUCK 😀
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+// const airline = 'TAP Air Portugal';
+// const plane = 'A320';
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Portugal'));
+
+// console.log(airline.slice(4));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+// const checkMiddleSeat = function (seatNo) {
+//   // B and E are middle seats.
+//   console.log(
+//     `${seatNo} is${
+//       seatNo.slice(-1) === 'B' || seatNo.slice(-1) === 'E' ? ' NOT' : ''
+//     } a middle seat`
+//   );
+// };
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
 const airline = 'TAP Air Portugal';
-const plane = 'A320';
 
-console.log(airline.indexOf('r'));
-console.log(airline.lastIndexOf('r'));
-console.log(airline.indexOf('Portugal'));
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
 
-console.log(airline.slice(4));
+// const passenger = 'yOuSeF';
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
 
-console.log(airline.slice(0, airline.indexOf(' ')));
-console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+// console.log(passengerCorrect);
 
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1));
+// // Comparing email?
+// const email = 'hello@yousef.com';
+// const loginEmail = '    hello@yousef.cOm  \n';
 
-const checkMiddleSeat = function (seatNo) {
-  // B and E are middle seats.
-  console.log(
-    `${seatNo} is${
-      seatNo.slice(-1) === 'B' || seatNo.slice(-1) === 'E' ? ' NOT' : ''
-    } a middle seat`
-  );
-};
+// const normalizedEmail = loginEmail.toLowerCase().trim();
 
-checkMiddleSeat('11B');
-checkMiddleSeat('23C');
-checkMiddleSeat('3E');
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
+
+// const announcement =
+//   'All passengers come to boarding door 23. Boarding door 23!';
+// console.log(announcement.replaceAll('door', 'gate'));
+
+const plane = 'A320neo';
+console.log(plane.includes('A320'));
+
+console.log(plane.startsWith('A3'));
