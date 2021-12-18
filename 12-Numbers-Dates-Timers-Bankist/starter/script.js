3bcaddf5dc471ff6c6b7b5179479ed1d56bc3ba3
yousefabughaidah <94529498+yousefabughaidah@users.noreply.github.com>
2021-12-18 20:18:47 +0300

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-12-10T17:01:17.194Z',
+    '2021-12-12T23:36:17.929Z',
+    '2021-12-15T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -81,6 +81,23 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (d1, d2) =>
+    Math.round(Math.abs(d2 - d1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return `Today`;
+  if (daysPassed === 1) return `Yesterday`;
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0); // it's zero based, so we're adding 1
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -92,10 +109,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0); // it's zero based, so we're adding 1
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -358,3 +372,14 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Date.now()); //output: 1639673365387 (miliseconds since jan 1 1970)
 // future.setFullYear(2040);
 // console.log(future);
+
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future);
+// console.log(+future);
+
+// const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1);
+// const d1 = new Date(2037, 3, 4);
+// const d2 = new Date(2037, 3, 14, 10, 8);
+
+// const days1 = calcDaysPassed(d1, d2) / (1000 * 60 * 60 * 24);
+// console.log(days1);
